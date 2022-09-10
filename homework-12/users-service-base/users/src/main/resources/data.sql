c001ba1d9564b581ca33ed6e693a3b0a17ebffe1
christina-gorina <christina.gorina@yandes.ru>
2022-09-11 00:19:28 +0300

    auth service change directory

--- homework-12/users-service-base/users/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO USERS (id, `name`, `password`, `email`, `first_name`, `last_name`, `phone` ) VALUES (1, 'admin', 'password', 'admin@gmail.com', 'adminFirstName', 'adminLastName', 'adminPhone');
-INSERT INTO USERS (id, `name`, `password`, `email`, `first_name`, `last_name`, `phone` ) VALUES (2, 'user', 'password', 'user@gmail.com', 'userFirstName', 'userLastName', 'userPhone');
